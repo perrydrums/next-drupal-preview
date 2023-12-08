5060186cc4f3cff7cc2054b76a22722913fd88d0
Perry Janssen <psperryjanssen@gmail.com>
2023-12-08 12:45:47 +0000

    fix: Inline css

--- src/DrupalPreviewBanner/index.tsx
+++ src/DrupalPreviewBanner/index.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { Edit, Close } from '../icons';
 import Link from 'next/link';
-import { useEffect, useState } from "react";
-import './styles.css';
 
 interface DrupalPreviewBannerProps {
   id: string
@@ -24,34 +23,69 @@ export default function DrupalPreviewBanner (props: DrupalPreviewBannerProps): R
   const mediaMatch = window.matchMedia('(max-width: 1024px)');
   const [matches, setMatches] = useState(mediaMatch.matches);
 
-  const color = props.textColor ?? '#232429';
-  const backgroundColor = props.backgroundColor ?? '#ffffff';
-  const buttonColor = props.buttonColor ?? '#003cc5';
-  const buttonSecondaryColor = props.buttonSecondaryColor ?? '#dc2323';
-  const buttonTextColor = props.buttonTextColor ?? '#ffffff';
-  const buttonSecondaryTextColor = props.buttonSecondaryTextColor ?? '#ffffff';
-  const borderColor = props.borderColor ?? '#919297';
-
   useEffect(() => {
     const handler = (e: MediaQueryListEvent): ReturnType<any> => { setMatches(e.matches) }
     mediaMatch.addEventListener('change', handler)
     return (): void => mediaMatch.removeEventListener('change', handler)
   });
 
+  const buttonStyle = {
+    backgroundColor: props.buttonColor ?? '#003cc5',
+    color: props.buttonTextColor ?? '#ffffff',
+    outline: 'none',
+    textDecoration: 'none',
+    fontSize: '1.4rem',
+    padding: 'calc(1rem - 1px) calc(1.5rem - 1px)',
+    alignItems: 'center',
+    minHeight: '4rem',
+    fontWeight: 700,
+    lineHeight: 1,
+    borderRadius: '2px',
+    transition: 'all 0.2s cubic-bezier(0.2, 0, 0, 1)',
+    flexShrink: 0,
+    display: 'inline-flex',
+    cursor: 'pointer',
+    marginRight: '2rem',
+  }
+
+  const buttonSecondaryStyle = {
+    backgroundColor: props.buttonSecondaryColor ?? '#dc2323',
+    color: props.buttonSecondaryTextColor ?? '#ffffff',
+    marginRight: 0,
+  }
+
   return (
-    <div className="drupal-preview-banner" style={{color}}>
-      <div className="grid">
-        <div className="inner-wrapper" style={{backgroundColor, outlineColor: borderColor}}>
-          <h5 className="title">Preview mode</h5>
+    <div style={{
+      color: props.textColor ?? '#232429',
+      position: 'fixed',
+      zIndex: 10,
+      bottom: '2.4rem',
+      left: 0,
+      right: 0,
+      fontFamily: "'-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'",
+    }}>
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: '1.6rem repeat(12, 1fr) 1.6rem',
+        gridColumnGap: '.8rem',
+      }}>
+        <div style={{
+          backgroundColor: props.backgroundColor ?? '#ffffff',
+          outline: `1px solid ${props.borderColor ?? '#919297'}`,
+          padding: '1.6rem',
+          gridColumn: '4/-4',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          borderRadius: '0.125rem',
+          backdropFilter: 'blur(13px)',
+        }}>
+          <h5 style={{margin: 0, fontSize: '2rem'}}>Preview mode</h5>
           <div>
             <Link
               href={`${props.cmsUrl}/node/${props.id}/edit`}
             >
-              <a
-                className="button"
-                style={{backgroundColor: buttonColor, color: buttonTextColor}}
-                type='button'
-              >
+              <a style={buttonStyle} type='button'>
                 {!matches && <>Edit this page in CMS &nbsp;</>}
                 <Edit />
               </a>
@@ -60,8 +94,7 @@ export default function DrupalPreviewBanner (props: DrupalPreviewBannerProps): R
               href={props.clearPreviewUrl ?? '/api/preview/clear'}
             >
               <a
-                className="button button--secondary"
-                style={{backgroundColor: buttonSecondaryColor, color: buttonSecondaryTextColor}}
+                style={{...buttonStyle, ...buttonSecondaryStyle}}
                 type='button'
               >
                 {!matches && <> Close preview mode &nbsp;</>}
--- src/icons/Edit/index.tsx
+++ src/icons/Edit/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { ReactSVGElement } from 'react';
 
-const Edit = (props: { size?: number|null, svg?: ReactSVGElement }): React.JSX.Element => {
-  const size = props.size ?? 24;
+const Edit = (props: { size?: number, color?: string, svg?: ReactSVGElement }): React.JSX.Element => {
+  const size = props.size ?? 18;
+  const color = props.color ?? '#FFFFFF';
   return props.svg ?? (
     <svg
       width={size}
@@ -13,15 +14,15 @@ const Edit = (props: { size?: number|null, svg?: ReactSVGElement }): React.JSX.E
     >
       <path
         d='M14.6626 2.87675L13.0766 1.29175C12.6876 0.90275 12.0516 0.90275 11.6626 1.29175L10.3696 2.58475L13.3696 5.58475L14.6626 4.29175C15.0516 3.90275 15.0516 3.26575 14.6626 2.87675Z'
-        fill='#FD2CA8'
+        fill={color}
       />
       <path
         d='M2.36714 10.5861L5.36694 13.5859L12.3655 6.58734L9.36574 3.58754L2.36714 10.5861Z'
-        fill='#FFE203'
+        fill={color}
       />
       <path
         d='M1.0256 14.6098C0.9386 14.8718 1.0806 15.0068 1.3416 14.9218L3.3426 14.2548L1.6926 12.6088L1.0256 14.6098Z'
-        fill='#000000'
+        fill={color}
       />
     </svg>
   );
